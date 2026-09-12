1ddf30b266b72730cfeaae071a943cf829d52092
Philippe Malo <me@philippemalo.dev>
2026-09-11 20:46:44 -0400

    Ajout URL du site prod.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://philippemalo.dev",
   integrations: [vue()],
   image: {
     domains: ["astro.build"],
