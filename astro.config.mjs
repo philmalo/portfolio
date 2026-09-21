9e3b95ae0cdd36b2717fadbc97362e94c83c0ecc
Philippe Malo <me@philippemalo.dev>
2026-09-21 16:55:49 -0400

    API native astro pour fonts, config native i18n, config markdown satteri

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,41 @@
-import { defineConfig } from 'astro/config';
-// import { satteri } from '@astrojs/markdown-satteri';
+import { defineConfig, fontProviders } from 'astro/config';
+import { satteri } from '@astrojs/markdown-satteri';
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  // fonts: [{
-  //   provider: fontProviders.local(),
-  //   name: 'JetBrainsMono',
-  //   cssVariable: '--font-jetbrains-mono',
-  //   options: {
-  //     variants: [{
-  //       weight: 'normal',
-  //       style: 'normal',
-  //       src: ['./src/styles/webfonts/JetBrainsMono-Regular.woff2']
-  //     }],
-  //   },
-  // }],
-  // i18n: {
-  //   locales: ["fr","en"],
-  //   defaultLocale: "fr",
-  //   routing: {
-  //     prefixDefaultLocale: false,
-  //   }
-  // },
+  fonts: [{
+    provider: fontProviders.local(),
+    name: 'JetBrainsMono',
+    cssVariable: '--font-jetbrains-mono',
+    options: {
+      variants: [{
+        weight: 'normal',
+        style: 'normal',
+        src: ['./src/styles/webfonts/JetBrainsMono-Regular.woff2']
+      }],
+    },
+  }],
+  i18n: {
+    locales: ["fr","en"],
+    defaultLocale: "fr",
+    routing: {
+      prefixDefaultLocale: false,
+    }
+  },
   image: {
     domains: ["astro.build"],
   },
   integrations: [react(), sitemap()],
-  // markdown: {
-  //   processor: satteri({
-  //     features: {
-  //       rawHtml: true,
-  //       smartPunctuation: true,
-  //     },
-  //   })
-  // },
+  markdown: {
+    processor: satteri({
+      features: {
+        rawHtml: true,
+        smartPunctuation: true,
+      },
+    })
+  },
   site: "https://philippemalo.dev",
   vite: {
     server: {
